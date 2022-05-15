f2ec74622909e3d8b9d7e44750c414135758e4a9
kimja01 <kimja7045@naver.com>
2022-05-15 18:28:23 +0900

    fix(facebook login): 페북 라이브러리를 jcenter에서 못 불러와서 실행이 안되어, 페북라이브러리 관련 주석 후, debug keystore를 클레이튼버전의 디버그 키를 가져와서 실행완료하였습니다.

--- src/view/LoginView.js
+++ src/view/LoginView.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-import { 
-    View, 
-    TouchableOpacity, 
-    Text, 
-    StyleSheet, 
-    Image,
-    ImageBackground 
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
 } from 'react-native';
 
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import Toast from 'react-native-toast-message';
 
-import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
-import auth from '@react-native-firebase/auth'
+// import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
+import auth from '@react-native-firebase/auth';
 
 import BackgroundImage from '@/assets/image/login_background.png';
 
@@ -23,162 +23,185 @@ import FacebookIcon from '@/assets/icon/facebook_icon.svg';
 import GoogleIcon from '@/assets/icon/google-icon.svg';
 
 const LoginView = ({clickHandler, isLogin}) => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '879653988026-78f2h0anqje9d3p7d0s8d5edooo4k9ub.apps.googleusercontent.com',
+    });
+    // GoogleSignin.configure({webClientId:'1:879653988026:android:a89e7bd80c796e90c24483'});
+  }, []);
 
-    useEffect(()=>{
-        GoogleSignin.configure({ webClientId: '879653988026-78f2h0anqje9d3p7d0s8d5edooo4k9ub.apps.googleusercontent.com' });
-        // GoogleSignin.configure({webClientId:'1:879653988026:android:a89e7bd80c796e90c24483'});
+  // const loginWithFacebook = () => {
+  //     LoginManager.logInWithPermissions(["public_profile", "email"]).then(
+  //       function(result) {
+  //         if (result.isCancelled) {
+  //             Toast.show({
+  //                 type: 'error',
+  //                 text1: '⚠ Alret',
+  //                 text2: 'Login was cancelled !! try again...',
+  //               });
+  //         } else {
+  //           AccessToken.getCurrentAccessToken().then((data) => {
+  //             const { accessToken } = data;
+  //             isLogin(accessToken);
+  //           });
+  //         }
+  //        },
+  //        function(error) {
+  //         console.log("==> Login fail with error: " + error);
+  //         Toast.show({
+  //             type: 'error',
+  //             text1: '⚠ Alret',
+  //             text2: 'Login Error !! check network status & try again...',
+  //           });
+  //        }
+  //      );
+  // };
 
-    },[]);
+  const onAuthStateChanged = user => {
+    console.log(user);
+  };
 
-    const loginWithFacebook = () => {
-        LoginManager.logInWithPermissions(["public_profile", "email"]).then(
-          function(result) {
-            if (result.isCancelled) {
-                Toast.show({
-                    type: 'error',
-                    text1: '⚠ Alret',
-                    text2: 'Login was cancelled !! try again...',
-                  });
-            } else {
-              AccessToken.getCurrentAccessToken().then((data) => {
-                const { accessToken } = data;
-                isLogin(accessToken);
-              });              
-            }
-           },
-           function(error) {
-            console.log("==> Login fail with error: " + error);
-            Toast.show({
-                type: 'error',
-                text1: '⚠ Alret',
-                text2: 'Login Error !! check network status & try again...',
-              });
-           }
-         );
-    };
+  const onGoogleButtonPress = async () => {
+    console.log('start google login');
+    try {
+      await GoogleSignin.hasPlayServices();
 
-    const onAuthStateChanged = (user) =>{
-        console.log(user);
+      const {idToken} = await GoogleSignin.signIn();
+      console.log(idToken);
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      auth().signInWithCredential(googleCredential);
+
+      auth().onAuthStateChanged(onAuthStateChanged);
+    } catch (err) {
+      console.log(err);
+      Toast.show({
+        type: 'error',
+        text1: '⚠ Alret',
+        text2: `${err}`,
+      });
     }
 
-    const onGoogleButtonPress = async () => { 
-        console.log('start google login');
-        try{
-            await GoogleSignin.hasPlayServices();
+    //return auth().signInWithCredential(googleCredential);
+  };
 
-            const { idToken } = await GoogleSignin.signIn(); 
-            console.log(idToken);
-    
-            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-            auth().signInWithCredential(googleCredential);
-
-            auth().onAuthStateChanged(onAuthStateChanged);
-
-        }catch(err){
-            console.log(err);
-            Toast.show({
-                type: 'error',
-                text1: '⚠ Alret',
-                text2: `${err}`,
-              });
-        }
-        
-        //return auth().signInWithCredential(googleCredential); 
-
-    };
-
-    return <View style={styles.container}>
-        <ImageBackground source={BackgroundImage}  style={{width:'100%',height:'100%'}}>
-
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={BackgroundImage}
+        style={{width: '100%', height: '100%'}}>
         <View style={styles.backgroundContainer}>
-                <Text style={styles.titleText}>CalorieCoin</Text>
-                <Text style={{color:'white', marginBottom:22, fontSize: 22, fontWeight:'100'}}>Play to <Text style={{fontWeight:'bold'}}>E</Text>xercise & <Text style={{fontWeight:'bold'}}>E</Text>arn !</Text>
-                <Text style={styles.subtitleText}>Exercise and Earn Money</Text>
-                <Text style={styles.subtitleText}>New Blockchain P2E Metaverse Skipping Rope Game</Text>
+          <Text style={styles.titleText}>CalorieCoin</Text>
+          <Text
+            style={{
+              color: 'white',
+              marginBottom: 22,
+              fontSize: 22,
+              fontWeight: '100',
+            }}>
+            Play to <Text style={{fontWeight: 'bold'}}>E</Text>xercise &{' '}
+            <Text style={{fontWeight: 'bold'}}>E</Text>arn !
+          </Text>
+          <Text style={styles.subtitleText}>Exercise and Earn Money</Text>
+          <Text style={styles.subtitleText}>
+            New Blockchain P2E Metaverse Skipping Rope Game
+          </Text>
 
-                <TouchableOpacity style={styles.googleLoginBtn} onPress={()=>{onGoogleButtonPress()}}>
-                    <GoogleIcon width={22} height={22} fill={'white'}/>
-                    <Text style={{fontSize:18, marginLeft:42, color : 'black'}}>Google Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.facebookLoginBtn} onPress={()=>{loginWithFacebook()}}>
-                    <FacebookIcon width={22} height={22} fill={'white'}/>
-                    <Text style={{fontSize:18, marginLeft:24, color : 'white'}}>Facebook Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.kakaoTalkLoginBtn} onPress={()=>{clickHandler()}}>
-                    <KakaoIcon width={22} height={22} fill={'black'}/>
-                    <Text style={{fontSize:18, marginLeft:24}}>Kakaotalk Login</Text>
-                </TouchableOpacity>
-            </View>
-        </ImageBackground>
-    </View>;
+          <TouchableOpacity
+            style={styles.googleLoginBtn}
+            onPress={() => {
+              onGoogleButtonPress();
+            }}>
+            <GoogleIcon width={22} height={22} fill={'white'} />
+            <Text style={{fontSize: 18, marginLeft: 42, color: 'black'}}>
+              Google Login
+            </Text>
+          </TouchableOpacity>
+          {/*<TouchableOpacity style={styles.facebookLoginBtn} onPress={()=>{loginWithFacebook()}}>*/}
+          {/*    <FacebookIcon width={22} height={22} fill={'white'}/>*/}
+          {/*    <Text style={{fontSize:18, marginLeft:24, color : 'white'}}>Facebook Login</Text>*/}
+          {/*</TouchableOpacity>*/}
+          <TouchableOpacity
+            style={styles.kakaoTalkLoginBtn}
+            onPress={() => {
+              clickHandler();
+            }}>
+            <KakaoIcon width={22} height={22} fill={'black'} />
+            <Text style={{fontSize: 18, marginLeft: 24}}>Kakaotalk Login</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container : {
-        backgroundColor:'white',
-        display: 'flex',
-        flex:1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    titleText: {
-        fontFamily:'Kanit-ExtraBoldItalic',
-        fontSize:44,
-        marginBottom:32,
-        color:'white'
-    },
-    backgroundImage: {
-        width:'100%',
-        height:'100%',
-        display: 'flex'
-    },
-    backgroundContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width:'100%', 
-        height:'100%'
-    },
-    subtitleText: {
-        color:'white',
-        fontFamily:'SUIT-Medium',
-        fontSize:14,
-        marginBottom:12,
-    },
-    kakaoTalkIcon:{
-      backgroundColor:'#3A1D1D'  
-    },
-    kakaoTalkLoginBtn:{
-        marginTop:20,
-        backgroundColor:'#FEE500',
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        padding: 14,
-        borderRadius:8,
-        width:'60%'
-    },
-    facebookLoginBtn:{
-        marginTop:20,
-        backgroundColor:'#4267B2',
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        padding: 14,
-        borderRadius:8,
-        width:'60%'
-    },
-    googleLoginBtn:{
-        marginTop:180,
-        backgroundColor:'#fff',
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-        padding: 14,
-        borderRadius:8,
-        width:'60%'
-    }
+  container: {
+    backgroundColor: 'white',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontFamily: 'Kanit-ExtraBoldItalic',
+    fontSize: 44,
+    marginBottom: 32,
+    color: 'white',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+  },
+  backgroundContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  subtitleText: {
+    color: 'white',
+    fontFamily: 'SUIT-Medium',
+    fontSize: 14,
+    marginBottom: 12,
+  },
+  kakaoTalkIcon: {
+    backgroundColor: '#3A1D1D',
+  },
+  kakaoTalkLoginBtn: {
+    marginTop: 20,
+    backgroundColor: '#FEE500',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 14,
+    borderRadius: 8,
+    width: '60%',
+  },
+  facebookLoginBtn: {
+    marginTop: 20,
+    backgroundColor: '#4267B2',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 14,
+    borderRadius: 8,
+    width: '60%',
+  },
+  googleLoginBtn: {
+    marginTop: 180,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 14,
+    borderRadius: 8,
+    width: '60%',
+  },
 });
 
 export default LoginView;
